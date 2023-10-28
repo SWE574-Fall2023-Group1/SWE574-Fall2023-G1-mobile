f9d03dc9a8cc4706b3f78f93443ad0377f5745ff
ayhancavdar <ayhancavdarkral@gmail.com>
2023-10-29 00:35:55 +0300

    [HomePage] Keep home page alive after page is change from bottom nav bar #32

--- mobile/lib/routes/home/home_route.dart
+++ mobile/lib/routes/home/home_route.dart
@@ -4,34 +4,36 @@ import 'package:memories_app/routes/home/bloc/home_bloc.dart';
 
 // TODO: Add home page design
 class HomeRoute extends StatefulWidget {
-  final HomeBloc bloc;
-  const HomeRoute({required this.bloc, super.key});
+  const HomeRoute({super.key});
 
   @override
   State<HomeRoute> createState() => _HomeRouteState();
 }
 
-class _HomeRouteState extends State<HomeRoute> {
+class _HomeRouteState extends State<HomeRoute>
+    with AutomaticKeepAliveClientMixin<HomeRoute> {
+  @override
+  bool get wantKeepAlive => true;
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<HomeBloc>(
-      create: (BuildContext context) => HomeBloc()..add(HomeLoadDisplayEvent()),
-      child: BlocConsumer<HomeBloc, HomeState>(
-        builder: (BuildContext context, HomeState state) {
-          Widget container;
-          if (state is HomeInitial) {
-            container = const CircularProgressIndicator();
-          } else if (state is HomeDisplayState) {
-            container = Container(
-              color: Colors.yellow,
-            );
-          } else {
-            container = const CircularProgressIndicator();
-          }
-          return container;
-        },
-        listener: (BuildContext context, HomeState state) {},
-      ),
+    super.build(context);
+    return BlocConsumer<HomeBloc, HomeState>(
+      builder: (BuildContext context, HomeState state) {
+        Widget container;
+        if (state is HomeInitial) {
+          container = const CircularProgressIndicator();
+        } else if (state is HomeDisplayState) {
+          // TODO: Show stories list
+          container = Container(
+            color: Colors.yellow,
+          );
+        } else {
+          container = const CircularProgressIndicator();
+        }
+        return container;
+      },
+      listener: (BuildContext context, HomeState state) {},
     );
   }
 }
