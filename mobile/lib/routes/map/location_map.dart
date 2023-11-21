fdeeabb770a72ca98e730c0792626fae1bea8108
ayhancavdar <ayhancavdarkral@gmail.com>
2023-11-21 22:01:13 +0300

    [Map] Add current location logic #60

--- mobile/lib/routes/map/location_map.dart
+++ mobile/lib/routes/map/location_map.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:memories_app/network/network_manager.dart';
 import 'package:memories_app/routes/map/location_list_tile.dart';
@@ -251,6 +254,52 @@ class _LocationMapState extends State<LocationMap> {
     }
   }
 
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      LatLng point = LatLng(position.latitude, position.longitude);
+      setState(() {
+        _addMarkerForPointSelection(point);
+      });
+      _mapController.move(point, _mapController.camera.zoom + 5);
+      //_reverseGeocode(position.latitude, position.longitude);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return _buildMapAndAdressList(context);
@@ -310,7 +359,7 @@ class _LocationMapState extends State<LocationMap> {
         Padding(
           padding: const EdgeInsets.all(SpaceSizes.x8),
           child: ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: _getCurrentPosition,
             icon: Image.asset(
               "assets/navigation.png",
               height: 16,
