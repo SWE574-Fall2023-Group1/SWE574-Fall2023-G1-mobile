37cb0e7d41b389f958aa24f6c3de8b58ca0b3a45
Mert Aydın <mertaydin97@gmail.com>
2023-12-30 21:23:25 +0300

    [Test] Add unit test for RegisterRepositoryImp #137

--- mobile/test/register_unit_test.dart
+++ mobile/test/register_unit_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:memories_app/network/network_manager.dart';
+import 'package:memories_app/routes/register/model/register_request_model.dart';
+import 'package:memories_app/routes/register/model/register_response_model.dart';
 import 'package:memories_app/routes/register/model/register_repository.dart';
+import 'package:memories_app/util/api_endpoints.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockNetworkManager extends Mock implements NetworkManager {}
@@ -14,9 +17,47 @@ void main() {
     registerRepository =
         RegisterRepositoryImp(networkManager: mockNetworkManager);
 
-    // Register fallback values for any types used in the mocked methods
     registerFallbackValue(Uri());
+    registerFallbackValue(RegisterRequestModel(
+      username: '',
+      email: '',
+      password: '',
+      passwordAgain: '',
+    ));
   });
 
-  group('RegisterRepositoryImp Tests', () {});
+  group('RegisterRepository Tests', () {
+    test('register calls correct endpoint and returns RegisterResponseModel',
+        () async {
+      final RegisterRequestModel testRequestModel = RegisterRequestModel(
+        username: '',
+        email: '',
+        password: '',
+        passwordAgain: '',
+      );
+      final RegisterResponseModel mockResponseModel = RegisterResponseModel(
+        success: true,
+        msg: 'ok',
+      );
+
+      // Convert RegisterResponseModel to a Map<String, dynamic>
+      final Map<String, dynamic> mockJson = mockResponseModel.toJson();
+
+      // Setup mock response for NetworkManager
+      when(() => mockNetworkManager.post(ApiEndpoints.register,
+              body: any(named: 'body')))
+          .thenAnswer((_) async => Result(mockJson, 200));
+
+      // Call register
+      final RegisterResponseModel responseModel =
+          await registerRepository.register(testRequestModel);
+
+      // Verify that the NetworkManager's post method was called with the correct URL and body
+      verify(() => mockNetworkManager.post(ApiEndpoints.register,
+          body: testRequestModel)).called(1);
+
+      // Check if the returned object is the same as the mock response
+      expect(responseModel.toJson(), equals(mockJson));
+    });
+  });
 }
