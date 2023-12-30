7f369514cdb0c35a8b60f3fe6e5189ca91892b9b
Mert Aydın <mertaydin97@gmail.com>
2023-12-30 21:23:35 +0300

    [Test] Add unit test for HomeRepositoryImp #137

--- mobile/test/home_unit_test.dart
+++ mobile/test/home_unit_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:memories_app/network/network_manager.dart';
+import 'package:memories_app/routes/home/model/response/stories_response_model.dart';
 import 'package:memories_app/routes/home/model/home_repository.dart';
+import 'package:memories_app/routes/home/model/story_model.dart';
+import 'package:memories_app/util/api_endpoints.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockNetworkManager extends Mock implements NetworkManager {}
@@ -11,12 +14,69 @@ void main() {
 
   setUpAll(() {
     mockNetworkManager = MockNetworkManager();
-    homeRepository =
-        HomeRepositoryImp(networkManager: mockNetworkManager);
+    homeRepository = HomeRepositoryImp(networkManager: mockNetworkManager);
 
-    // Register fallback values for any types used in the mocked methods
     registerFallbackValue(Uri());
   });
 
-  group('HomeRepositoryImp Tests', () {});
+  group('HomeRepository Tests', () {
+    test(
+        'getUserStories calls correct endpoint and returns StoriesResponseModel',
+        () async {
+      const int testPage = 1;
+      const int testSize = 10;
+      final StoriesResponseModel mockResponseModel = StoriesResponseModel(
+        stories: <StoryModel>[],
+        hasNext: null,
+        hasPrev: null,
+        nextPage: null,
+        prevPage: null,
+        totalPages: null,
+      );
+
+      final Map<String, dynamic> mockJson = mockResponseModel.toJson();
+
+      when(() => mockNetworkManager
+              .get(ApiEndpoints.buildUserStoriesUrl(testPage, testSize)))
+          .thenAnswer((_) async => Result(mockJson, 200));
+
+      final StoriesResponseModel responseModel =
+          await homeRepository.getUserStories(page: testPage, size: testSize);
+
+      verify(() => mockNetworkManager
+          .get(ApiEndpoints.buildUserStoriesUrl(testPage, testSize))).called(1);
+
+      expect(responseModel.toJson(), equals(mockJson));
+    });
+
+    test(
+        'getAllStoriesWithOwnUrl calls correct endpoint and returns StoriesResponseModel',
+        () async {
+      const int testPage = 1;
+      const int testSize = 10;
+      final StoriesResponseModel mockResponseModel = StoriesResponseModel(
+        stories: <StoryModel>[],
+        hasNext: null,
+        hasPrev: null,
+        nextPage: null,
+        prevPage: null,
+        totalPages: null,
+      );
+
+      final Map<String, dynamic> mockJson = mockResponseModel.toJson();
+
+      when(() => mockNetworkManager
+              .get(ApiEndpoints.buildAllStoriesWithOwnUrl(testPage, testSize)))
+          .thenAnswer((_) async => Result(mockJson, 200));
+
+      final StoriesResponseModel responseModel = await homeRepository
+          .getAllStoriesWithOwnUrl(page: testPage, size: testSize);
+
+      verify(() => mockNetworkManager
+              .get(ApiEndpoints.buildAllStoriesWithOwnUrl(testPage, testSize)))
+          .called(1);
+
+      expect(responseModel.toJson(), equals(mockJson));
+    });
+  });
 }
