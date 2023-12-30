5f9e1c699f4a2815b0b846049a0a2a3407b6f6ac
Mert Aydın <mertaydin97@gmail.com>
2023-12-30 19:27:29 +0300

    [Test] Add unit test for date_util #119

--- mobile/test/date_util_test.dart
+++ mobile/test/date_util_test.dart
@@ -20,7 +20,7 @@ void main() {
         year: 2015,
         startYear: null,
         endYear: null,
-        date: null,
+        date: "2023-12-23T19:46:55.656341Z",
         creationDate: DateTime.parse("2023-12-23T19:46:55.656341Z").toLocal(),
         startDate: null,
         endDate: null,
@@ -30,12 +30,30 @@ void main() {
         dateText: null,
       );
       expect(getFormattedDate(story), 'Year: 2015');
+      story.dateType = "decade";
+      story.decade = 2015;
+      expect(getFormattedDate(story), 'Decade: 2015');
+      story.dateType = "year_interval";
+      story.startYear = 2015;
+      story.endYear = 2020;
+      expect(getFormattedDate(story), 'Start: 2015 \nEnd: 2020');
+      story.dateType = "normal_date";
+      expect(getFormattedDate(story), formatDate(story.date));
+      story.dateType = "interval_date";
+      story.startDate = "2023-12-23T19:46:55.656341Z";
+      story.endDate = "2023-12-30T19:46:55.656341Z";
+      expect(getFormattedDate(story),
+          'Start: ${formatDate(story.startDate)} \nEnd: ${formatDate(story.endDate)}');
     });
   });
 
   group('formatDate', () {
     test('should format valid date string correctly', () {
       expect(formatDate('2023-12-25'), '2023-12-25');
+    });
+
+    test('should format valid date string correctly', () {
+      expect(formatDate('2023-12-23T00:00:00.000000Z'), '2023-12-23');
     });
 
     test('should return null for invalid date string', () {
