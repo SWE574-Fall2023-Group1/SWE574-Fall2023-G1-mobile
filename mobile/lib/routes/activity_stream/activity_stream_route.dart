e823844ef1d80890e91d4a10b97330473a061521
aykutkantas <126410955+aykutkantas@users.noreply.github.com>
2023-12-30 16:20:55 +0300

    [Suggestion] Add activity stream design #110

--- mobile/lib/routes/activity_stream/activity_stream_route.dart
+++ mobile/lib/routes/activity_stream/activity_stream_route.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:memories_app/routes/activity_stream/bloc/activity_stream_bloc.dart';
 import 'package:memories_app/routes/activity_stream/model/activity_stream_response_model.dart';
-import 'package:memories_app/util/router.dart';
 import 'package:memories_app/util/styles.dart';
+import 'package:intl/intl.dart';
+import 'package:memories_app/util/router.dart';
 
 class ActivityStreamRoute extends StatefulWidget {
-  const ActivityStreamRoute({super.key});
+  const ActivityStreamRoute({Key? key}) : super(key: key);
 
   @override
   State<ActivityStreamRoute> createState() => _ActivityStreamRouteState();
@@ -17,297 +18,98 @@ class _ActivityStreamRouteState extends State<ActivityStreamRoute> {
   Widget build(BuildContext context) {
     return BlocBuilder<ActivityStreamBloc, ActivityStreamState>(
       builder: (BuildContext context, ActivityStreamState state) {
-        Widget column;
+        Widget content;
+        List<Activity> activities = [];
+
         if (state is ActivityStreamInitial) {
-          column = const Center(child: CircularProgressIndicator());
+          content = const Center(child: CircularProgressIndicator());
         } else if (state is ActivityStreamDisplayState) {
-          column = state.allActivities.isNotEmpty
-              ? SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      if (state.followedUser.isNotEmpty)
-                        _buildFollowedUserActivities(state.followedUser),
-                      if (state.unfollowedUser.isNotEmpty)
-                        _buildUnfollowedUserActivities(state.unfollowedUser),
-                      if (state.commentOnStory.isNotEmpty)
-                        _buildCommentOnStoryActivities(state.commentOnStory),
-                      if (state.commentStoryYouCommentedBefore.isNotEmpty)
-                        _buidCommentStoryYouCommentedBeforeActivities(
-                            state.commentStoryYouCommentedBefore),
-                      if (state.likedStories.isNotEmpty)
-                        _buildLikedStoryActivities(state.likedStories),
-                      if (state.newStories.isNotEmpty)
-                        _buildNewStoryActivities(state.newStories),
-                    ],
-                  ),
-                )
-              : const Center(
-                  child: Text("There is no recent activity for you"),
-                );
+          activities
+            ..addAll(state.likedStories)
+            ..addAll(state.unlikedStories)
+            ..addAll(state.followedUser)
+            ..addAll(state.unfollowedUser)
+            ..addAll(state.commentOnStory)
+            ..addAll(state.commentStoryYouCommentedBefore)
+            ..addAll(state.newStories);
+
+          activities.sort((a, b) => b.date.compareTo(a.date));
+
+          content = ListView.builder(
+            itemCount: activities.length,
+            itemBuilder: (context, index) => _buildActivityItem(context, activities[index]),
+          );
         } else if (state is ActivityStreamFailure) {
-          column = Center(
-            child: Text("Error: ${state.error.toString()}"),
-          );
+          content = Center(child: Text("Error: ${state.error.toString()}"));
         } else if (state is ActivityStreamOffline) {
-          column = Center(
-            child: Text(state.offlineMessage.toString()),
-          );
+          content = Center(child: Text(state.offlineMessage ?? 'You are offline.'));
         } else {
-          column = const Center(child: CircularProgressIndicator());
+          content = const Center(child: CircularProgressIndicator());
         }
 
-        return Material(
-          child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              centerTitle: true,
-              title: const Text(
-                "Activity Stream",
-                style: Styles.appBarTitleStyle,
-              ),
-            ),
-            body: column,
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            iconTheme: IconThemeData(color: Colors.black),
+            centerTitle: true,
+            title: const Text("Activity Stream", style: Styles.appBarTitleStyle),
           ),
+          body: content,
         );
       },
     );
   }
 
-  Widget _buildFollowedUserActivities(List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Followed You",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title: Text("${activity.targetUserUsername} followed you!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
+  Widget _buildActivityItem(BuildContext context, Activity activity) {
+    IconData icon = _getActivityIcon(activity.activityType);
+    String formattedDate = DateFormat('dd/MM/yyyy HH:mm').format(activity.date);
+    String titleText = "${activity.targetUserUsername} $formattedDate";
+    return ListTile(
+      title: Text(titleText),
+      subtitle: Text(_getActivityDescription(activity)),
+      trailing: Icon(icon),
+      onTap: () {
+        AppRoute.profile
+            .navigate(context, arguments: activity.targetUser);
+        BlocProvider.of<ActivityStreamBloc>(context).add(
+            ActivityStreamOnPressActivityEvent(
+                activityId: activity.id));
+      },
     );
   }
 
-  Widget _buildUnfollowedUserActivities(List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Unfollowed You",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title: Text("${activity.targetUserUsername} unfollowed you!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
-    );
+  IconData _getActivityIcon(String activityType) {
+    switch (activityType) {
+      case 'story_liked':
+        return Icons.favorite;
+      case 'story_unliked':
+        return Icons.favorite_border;
+      case 'followed_user':
+        return Icons.person_add;
+      case 'unfollowed_user':
+        return Icons.person_remove;
+      case 'new_story':
+        return Icons.create;
+      default:
+        return Icons.info;
+    }
   }
 
-  Widget _buildCommentOnStoryActivities(List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Commented on Your Story",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title: Text(
-                    "${activity.targetUserUsername} commented on your story!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
-    );
-  }
-
-  Widget _buidCommentStoryYouCommentedBeforeActivities(
-      List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Commented a Story You Commented Before",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title: Text(
-                    "${activity.targetUserUsername} commented a story you commented before!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
-    );
-  }
-
-  Widget _buildLikedStoryActivities(List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Liked Your Story",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title: Text("${activity.targetUserUsername} liked your story!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
-    );
-  }
-
-  Widget _buildNewStoryActivities(List<Activity> activities) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            "Users Created New Stories",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        // Section Activities
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: activities.length,
-          itemBuilder: (BuildContext context, int subIndex) {
-            Activity activity = activities[subIndex];
-            return GestureDetector(
-              onTap: () {
-                AppRoute.profile
-                    .navigate(context, arguments: activity.targetUser);
-                BlocProvider.of<ActivityStreamBloc>(context).add(
-                    ActivityStreamOnPressActivityEvent(
-                        activityId: activity.id));
-              },
-              child: ListTile(
-                title:
-                    Text("${activity.targetUserUsername} created a new story!"),
-                // Add more ListTile customization as needed
-              ),
-            );
-          },
-        ),
-        // Divider between sections
-        const Divider(),
-      ],
-    );
+  String _getActivityDescription(Activity activity) {
+    switch (activity.activityType) {
+      case 'story_liked':
+        return 'Liked "${activity.targetStoryTitle}"';
+      case 'story_unliked':
+        return 'Unliked "${activity.targetStoryTitle}"';
+      case 'followed_user':
+        return 'Followed you';
+      case 'unfollowed_user':
+        return 'Unfollowed you';
+      case 'new_story':
+        return 'Created a new story: "${activity.targetStoryTitle}"';
+      default:
+        return 'Unknown activity';
+    }
   }
 }
+
