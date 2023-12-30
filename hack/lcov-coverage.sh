ebb879bd056822816e54ec9b6d3219aa6c91f968
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-31 00:33:28 +0300

    [Test] Update lcov remove list #137

--- hack/lcov-coverage.sh
+++ hack/lcov-coverage.sh
@@ -9,16 +9,20 @@ lcov \
   --remove coverage/lcov.info \
   -o coverage/lcov_group1.info \
   'lib/network/*' \
-  'lib/routes/login/bloc/*_event.dart' \
-  'lib/routes/login/bloc/*_state.dart' \
+  'lib/util/*' \
+  'lib/routes/*/bloc/*_event.dart' \
+  'lib/routes/*/bloc/*_state.dart' \
+  'lib/routes/*/model/request/*' \
+  'lib/routes/*/model/response/*' \
   'lib/routes/login/model/*' \
   'lib/routes/register/bloc/*' \
   'lib/routes/register/model/*model*.dart' \
   'lib/routes/register/register_route.dart' \
-  'lib/util/*' \
+  'lib/routes/edit_story/model/*' \
+  'lib/routes/recommendations/model/*' \
+  'lib/routes/search/model/*' \
+  'lib/routes/activity_stream/model/*' \
   'lib/routes/profile/bloc/*' \
-  'lib/routes/profile/model/request/*' \
-  'lib/routes/profile/model/response/*' \
   'lib/routes/profile/widget' \
   'lib/routes/profile/profile_route.dart' \
   'lib/routes/home/model/response/*' \
@@ -27,7 +31,9 @@ lcov \
   'lib/routes/story_detail/model/request/*' \
   'lib/routes/story_detail/model/*model*.dart' \
   'lib/routes/story_detail/widget/*' \
-  'lib/routes/story_detail/story_detail_route.dart'
+  'lib/routes/story_detail/story_detail_route.dart' \
+  'lib/routes/create_story/model/*' \
+  'lib/routes/create_story/create_story_repository.dart'
 
 genhtml coverage/lcov_group1.info \
   --output=coverage \
